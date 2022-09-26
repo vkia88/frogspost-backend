7292190c47e40b53e977f5874b85aa6ebdeb2ce2
vkia88 <vkia88@inf.elte.hu>
2022-09-26 02:29:12 +0200

    init

---
+++ src/main/java/hu/frogspost/frogspost/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hu.frogspost.frogspost.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello world!";
+    }
+
+}
