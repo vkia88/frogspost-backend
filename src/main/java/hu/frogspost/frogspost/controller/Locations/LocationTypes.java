4a430938d4b0e45c0716b3ae16439a1a7ff0ea04
vkia88 <vkia88@inf.elte.hu>
2022-09-26 15:21:50 +0200

    adding locations

---
+++ src/main/java/hu/frogspost/frogspost/controller/Locations/LocationTypes.java
@@ -0,0 +1,19 @@
+package hu.frogspost.frogspost.controller.Locations;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@JsonFormat(shape=JsonFormat.Shape.OBJECT)
+@AllArgsConstructor
+@Getter
+public enum LocationTypes implements Serializable {
+    Budapest("Budapest", 1),
+    Szeged("Szeged", 2),
+    Debrecen("Debrecen", 3);
+
+    private String name;
+    private int id;
+}
